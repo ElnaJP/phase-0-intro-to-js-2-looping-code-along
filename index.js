66e365b1439f5074afefdad319c0806057a9ec27
Elna JP <eriosdd@gmail.com>
2022-05-29 14:47:17 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Code your solutions in this file
+const names = ["Guadalupe", "Ollie", "Aki"];
+console.log(names["Guadalupe", "Ollie", "Aki"]);
+names["Guadalupe", "Ollie", "Aki" ];
+
+const writeCards = () =>  {
+    let message = [];
+  names.forEach((name) => {
+    message.push(`Thank you, ${name}, for the wonderful surprise gift!`);
+  });
+  return message;
+}
+console.log(writeCards());
+
+ countDown = (number) => {
+    for(number = 10;
+    number >= 0; number--) 
+    console.log(number);
+}
+
+ countDown = (number) => {
+    for(number = 4;
+    number >= 0; number--) 
+    console.log(number);
+}
+
+
+
+    
